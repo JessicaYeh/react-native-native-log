8b7009949808bf745367e59de3d7e3b663dd3f4a
Jessica Yeh <jessicayeh@gmail.com>
2020-07-24 16:18:08 -0500

    fix: warning about requiresMainQueueSetup

--- ios/NativeLog.m
+++ ios/NativeLog.m
@@ -2,6 +2,11 @@
 
 @interface RCT_EXTERN_MODULE(NativeLog, NSObject)
 
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+
 RCT_EXTERN_METHOD(log:(NSString *)message)
 
 RCT_EXTERN_METHOD(logWithTag:(NSString *)tag message:(NSString *)message)
